e97e84b90843af912442f368a2f256e6822fa367
sukeshalex <sukeshsss200@gmail.com>
2023-07-20 22:33:44 +0530

    5th commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,15 +77,15 @@ themeToggleBtn.addEventListener("click", function () {
   elemToggleFunc(themeToggleBtn);
 
   if (themeToggleBtn.classList.contains("active")) {
-    document.body.classList.remove("dark_theme");
-    document.body.classList.add("light_theme");
-
-    localStorage.setItem("theme", "light_theme");
-  } else {
-    document.body.classList.add("dark_theme");
     document.body.classList.remove("light_theme");
+    document.body.classList.add("dark_theme");
 
     localStorage.setItem("theme", "dark_theme");
+  } else {
+    document.body.classList.add("light_theme");
+    document.body.classList.remove("dark_theme");
+
+    localStorage.setItem("theme", "light_theme");
   }
 
 });
@@ -94,12 +94,12 @@ themeToggleBtn.addEventListener("click", function () {
  * check & apply last time selected theme from localStorage
  */
 
-if (localStorage.getItem("theme") === "light_theme") {
+if (localStorage.getItem("theme") === "dark_theme") {
   themeToggleBtn.classList.add("active");
-  document.body.classList.remove("dark_theme");
-  document.body.classList.add("light_theme");
-} else {
-  themeToggleBtn.classList.remove("active");
   document.body.classList.remove("light_theme");
   document.body.classList.add("dark_theme");
+} else {
+  themeToggleBtn.classList.remove("active");
+  document.body.classList.remove("dark_theme");
+  document.body.classList.add("light_theme");
 }
